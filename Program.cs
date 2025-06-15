e4ba4cc256553b4a0ce53ca7cfc5c97ae025edf1
Gabi Thaler <gmth054@gmail.com>
2025-06-15 12:17:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace investigationGameApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
